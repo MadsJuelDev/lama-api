5c532bec45392efbb7aaf3531651eed4b5379ad0
MadsJuelDev <92512864+MadsJuelDev@users.noreply.github.com>
2022-05-20 13:37:56 +0200

    fixed Next  model to mapArry

--- routes/NextWeek.js
+++ routes/NextWeek.js
@@ -67,6 +67,11 @@ function mapProdArray(obj) {
   let outputArr = obj.map((element) => ({
     id: element._id,
     archived: element.archived,
+    isCollapsed: element.isCollapsed,
+    date: element.date,
+    description: element.description,
+    urgency: element.urgency,
+    status: element.status,
     projectId: element.projectId,
     task: element.task,
     userId: element.userId,
