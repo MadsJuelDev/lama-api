b8d313b7b313517b36453155d54d350f74902d66
MadsJuelDev <92512864+MadsJuelDev@users.noreply.github.com>
2022-06-12 14:53:11 +0200

    unity test

--- routes/Auth.js
+++ routes/Auth.js
@@ -54,7 +54,7 @@ router.post("/login", async (req, res) => {
   //validate user login info
   const { error } = loginValidation(req.body);
   if (error) {
-    return res.status(400).json({ error: error.details[0].message });
+    return res.status(418).json({ error: error.details[0].message });
   }
 
   //if info is valid find user
